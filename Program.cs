ab68c3294b307cccde00d70b4380cc97529c8a18
fevaavef <dvk.09@mail.ru>
2019-09-02 22:45:18 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumFunc
+{
+    public class FuncClass
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static int Sum(int[] input)
+        {
+            int m1, m2;
+            if (input.Length == 0)
+            {
+                Console.WriteLine("Array is empty");
+                return -1;
+            }
+            m1 = input[0];
+            m2 = input[1];
+            for (int i = 2; i < input.Length; i++)
+            {
+                if (m2 < m1)
+                {
+                    int a = m2;
+                    m2 = m1;
+                    m1 = a;
+                }
+                if (input[i] < m2)
+                    m2 = input[i];
+            }
+            return m1+m2;
+        }
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SumFunc;
+
+namespace ThreeTestMethods
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int res = FuncClass.Sum(new int[] { 4, 0, 3, 19, 492, -10, 1 });
+            int expected = -10;
+            Assert.AreEqual(expected, res, 0.001, "Wrong result");
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int res = FuncClass.Sum(new int[] { });
+            int expected = -1;
+            Assert.AreEqual(expected, res, 0.001, "Wrong result when array is empty");
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int res = FuncClass.Sum(new int[] { -435225, -666520, 330148, 32124, -982125, 642548, -232883, 93953, -145405 
+                -410975, -342179, 14223, -11750, -106185, 365119, -504492, -13995, 754063, -838410,
+                285348, 317867, -637511, -286379, -204314, -866566, -795777, 511382, -885599, - 452393, -654165, 225923, 
+                298815, -134432, 977892, -504113, -39959, 350042, 924676, 688286, -145484, 302951, 581063, -625195, -942474, 
+                -380914, -217358, 25675, -666575, 153506, -333397 });
+            int expected = -1924599;
+            Assert.AreEqual(expected, res, 0.001, "Wrong result when array is empty");
+        }
+    }
+}
